4e0b4b3051383142bcc858143c5093d5b6d4f3ad
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-04-20 11:35:15 +0700

    switch case for main character status

--- Agent-069/src/mad/agent069/MainCharacter.java
+++ Agent-069/src/mad/agent069/MainCharacter.java
@@ -8,7 +8,7 @@ public class MainCharacter {
 	// The initial position of the main character
 	public static final float ORIGINAL_X = 20;
 	public static final float ORIGINAL_Y = 20;
-	
+
 	// The current position of the main character
 	private float currentX;
 	private float currentY;
@@ -18,47 +18,84 @@ public class MainCharacter {
 	public static final int CURRENT_STATUS_NORMAL = 1;
 	public static final int CURRENT_STATUS_JUMPING = 2;
 	public static final int CURRENT_STATUS_LOWERHEAD = 3;
-	
+
 	// The current texture of the main character
 	private Texture currentTexture;
-	
+
 	// The texture to draw for normal status
 	private Texture normalTexture;
-	
+
 	// The texture to draw for lower head status
 	private Texture lowerheadTexture;
-	
+
 	// The current scene this main character belongs to
 	private Scene currentScene;
-	
-	public MainCharacter(Scene currentScene){
+
+	public MainCharacter(Scene currentScene) {
 		// The current scene
 		this.currentScene = currentScene;
-		
+
 		// Init the normal texture
-		this.normalTexture = new Texture(Gdx.files.internal("maincharacter/player-normal.png"));
-		
+		this.normalTexture = new Texture(
+				Gdx.files.internal("maincharacter/player-normal.png"));
+
 		// Init the lowerhead texture
-		this.lowerheadTexture = new Texture(Gdx.files.internal("maincharacter/player-low.png"));
-		
+		this.lowerheadTexture = new Texture(
+				Gdx.files.internal("maincharacter/player-low.png"));
+
 		// Set the current status and the current texture
 		this.currentStatus = MainCharacter.CURRENT_STATUS_NORMAL;
 		this.currentTexture = this.normalTexture;
-		
+
 		// Set the current position of the main character
 		this.currentX = MainCharacter.ORIGINAL_X;
 		this.currentY = MainCharacter.ORIGINAL_Y;
 	}
-	
+
 	/**
 	 * Draw the main character
 	 * 
-	 * @param batch The SpriteBatch of the current Scene
+	 * @param batch
+	 *            The SpriteBatch of the current Scene
 	 */
-	public void drawMainCharacter(SpriteBatch batch){
+	public void drawMainCharacter(SpriteBatch batch) {
+		switch (this.currentStatus) {
+		case MainCharacter.CURRENT_STATUS_NORMAL:
+			this.currentStatusNormalHandler();
+			break;
+			
+		case MainCharacter.CURRENT_STATUS_LOWERHEAD:
+			this.currentStatusLowerheadHandler();
+			break;
+			
+		default:
+			this.currentStatusJumpingHandler();
+			break;
+		}
 		batch.draw(this.currentTexture, this.currentX, this.currentY);
 	}
 	
+	/**
+	 * Handler for normal status
+	 */
+	private void currentStatusNormalHandler(){
+		this.currentTexture = this.normalTexture;
+	}
+	
+	/**
+	 * Handler for jumping status
+	 */
+	private void currentStatusJumpingHandler(){
+		this.currentTexture = this.normalTexture;
+	}
+	
+	/**
+	 * Handler for lower head status
+	 */
+	private void currentStatusLowerheadHandler(){
+		this.currentTexture = this.lowerheadTexture;
+	}
+
 	public int getCurrentStatus() {
 		return currentStatus;
 	}
@@ -94,12 +131,15 @@ public class MainCharacter {
 	public float getCurrentX() {
 		return currentX;
 	}
+
 	public void setCurrentX(float currentX) {
 		this.currentX = currentX;
 	}
+
 	public float getCurrentY() {
 		return currentY;
 	}
+
 	public void setCurrentY(float currentY) {
 		this.currentY = currentY;
 	}
