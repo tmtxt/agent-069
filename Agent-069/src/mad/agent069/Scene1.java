5a754b6c253b7c7995ddfc5a8b5d49291414525b
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-04-20 10:23:38 +0700

    finish create()

--- Agent-069/src/mad/agent069/Scene1.java
+++ Agent-069/src/mad/agent069/Scene1.java
@@ -1,14 +1,24 @@
 package mad.agent069;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Scene1 extends Scene {
 	
 	// The track texture
 	private Texture trackTexture;
 	
+	// Current track position
+	private float currentTrackX;
+	
 	// The sky texture
 	private Texture skyTexture;
+	
+	// Current sky position
+	private float currentSkyX;
 
 	@Override
 	public void create() {
@@ -21,7 +31,20 @@ public class Scene1 extends Scene {
 		// The main character
 		this.mainCharacter = new MainCharacter(this);
 		
+		// Init the camera
+		this.camera = new OrthographicCamera();
+		this.camera.setToOrtho(false, Scene.SCENE_WIDTH, Scene.SCENE_HEIGHT);
 		
+		// Init the sprite batch
+		this.batch = new SpriteBatch();
+		
+		// Init the texture to draw the track
+		this.trackTexture = new Texture(Gdx.files.internal("scene1track.png"));
+		this.currentTrackX = Scene.SCENE_WIDTH;
+		
+		// Init the texture to draw the sky
+		this.skyTexture = new Texture(Gdx.files.internal("scene1sky.png"));
+		this.currentSkyX = Scene.SCENE_HEIGHT;
 	}
 
 	@Override
@@ -34,6 +57,12 @@ public class Scene1 extends Scene {
 	public void render() {
 		// TODO Auto-generated method stub
 		super.render();
+		
+		// Clear color
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		
 	}
 
 	@Override
