4d156a09ff9cbff0d5ff5622c9f75e1ee4fa4785
Tran Xuan Truong <tranxuantruong@MacBook-Pro.local>
2013-05-02 21:07:50 +0700

    edit tank slow obstacle

--- Agent-069/src/mad/agent069/obstacles/ObstacleTexture.java
+++ Agent-069/src/mad/agent069/obstacles/ObstacleTexture.java
@@ -9,6 +9,7 @@ public class ObstacleTexture {
 	public static final Texture HELI_OBSTACLE_TEXTURE = new Texture(Gdx.files.internal("heli.png"));
 	public static final Texture ROCK_OBSTACLE_TEXTURE = new Texture(Gdx.files.internal("stone.png"));
 	public static final Texture TANK_OBSTACLE_TEXTURE = new Texture(Gdx.files.internal("tank.png"));
+	public static final Texture TANK_OBSTACLE_SLOW_TEXTURE = new Texture(Gdx.files.internal("tankslow.png"));
 	public static final Texture TANK_COLLAPSE_OBSTACLE_TEXTURE = new Texture(Gdx.files.internal("tankcollapse.png"));
 	public static final Texture THORNS_OBSTACLE_TEXTURE = new Texture(Gdx.files.internal("thorns.png"));
 	public static final Texture ROCKET_OBSTACLE_TEXTURE = new Texture(Gdx.files.internal("rocket.png"));
