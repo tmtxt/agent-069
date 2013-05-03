477fcec516df3bb785e4f8178067898cb8055915
Tran Xuan Truong <tranxuantruong@macbook-pro.rmit.edu.vn>
2013-05-03 13:44:34 +0700

    fix scene time

--- Agent-069/src/mad/agent069/Scene1.java
+++ Agent-069/src/mad/agent069/Scene1.java
@@ -43,7 +43,7 @@ public class Scene1 extends Scene {
 		this.speed = (float) 1;
 		
 		// Init the length
-		this.length = 1000;
+		this.length = 15000;
 
 		// Init the background texture to draw
 		this.backgroundTexture = new Texture(Gdx.files.internal("factory.png"));
